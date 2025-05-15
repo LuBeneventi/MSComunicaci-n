7efb74549e0782df366fc87eda9d58f5638724cb
LuBeneventi <lu.beneventi@duocuc.cl>
2025-05-15 16:41:56 -0400

    Mejoras controller

--- src/main/java/com/MSResena/Soporte/Comunicacion/controller/ReseñaController.java
+++ src/main/java/com/MSResena/Soporte/Comunicacion/controller/ReseñaController.java
@@ -3,6 +3,7 @@ package com.MSResena.Soporte.Comunicacion.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,29 +16,41 @@ import com.MSResena.Soporte.Comunicacion.model.resena;
 import com.MSResena.Soporte.Comunicacion.service.resenaService;
 
 @RestController
-@RequestMapping("api/Reseñas")
+@RequestMapping("api/reseñas")
 public class ReseñaController {
     
     @Autowired
     private resenaService rService;
 
-    @PostMapping("/Comentar")
+    @PostMapping("/comentar")
     public ResponseEntity<resena> guardar(@RequestBody resena res) {
         return ResponseEntity.ok(rService.guardar(res));
     }
 
     @GetMapping
     public ResponseEntity<List<resena>> listarTodas() {
+        List<resena> resena = rService.listar();
+        if(resena.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
         return ResponseEntity.ok(rService.listar());
     }
 
-    @GetMapping("/Producto: {idProducto}")
+    @GetMapping("/producto/{idProducto}")
     public ResponseEntity<List<resena>> buscarPorProducto(@PathVariable int idProducto) {
+        List<resena> producto = rService.buscarPorProducto(idProducto);
+        if(producto.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
         return ResponseEntity.ok(rService.buscarPorProducto(idProducto));
     }
 
-    @GetMapping("/Cliente: {idCliente}")
+    @GetMapping("/cliente/{idCliente}")
     public ResponseEntity<List<resena>> buscarPorCliente(@PathVariable int idCliente) {
+         List<resena> cliente = rService.buscarPorCliente(idCliente);
+         if(cliente.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+         }
         return ResponseEntity.ok(rService.buscarPorCliente(idCliente));
     }
 }
--- src/main/java/com/MSResena/Soporte/Comunicacion/controller/soporteController.java
+++ src/main/java/com/MSResena/Soporte/Comunicacion/controller/soporteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,22 +25,26 @@ public class soporteController {
     @Autowired
     private soporteService sService;
 
-    @PostMapping("/Comentar")
+    @PostMapping("/comentar")
     public ResponseEntity<solicitudSoporte> crear(@RequestBody solicitudSoporte solicitud) {
         return ResponseEntity.ok(sService.crearSolicitud(solicitud));
     }
 
     @GetMapping
     public ResponseEntity<List<solicitudSoporte>> listar() {
+        List<solicitudSoporte> solicitudes = sService.listar();
+        if(solicitudes.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
         return ResponseEntity.ok(sService.listar());
     }
 
-    @PutMapping("/{id}/Cerrar")
+    @PutMapping("/cerrar/{id}")
     public ResponseEntity<solicitudSoporte> cerrar(@PathVariable int id) {
         return ResponseEntity.ok(sService.cerrar(id));
     }
 
-    @PostMapping("/{id}/Responder")
+    @PostMapping("/responder/{id}")
     public ResponseEntity<RespuestaSoporte> responder(
             @PathVariable int id,
             @RequestBody RespuestaSoporte respuesta) {
@@ -51,8 +56,12 @@ public class soporteController {
         return ResponseEntity.ok(sService.buscarPorId(id));
     }
 
-    @GetMapping("/Cliente: {idCliente}")
+    @GetMapping("/cliente/{idCliente}")
     public ResponseEntity<List<solicitudSoporte>> buscarPorCliente(@PathVariable int idCliente){
+        List<solicitudSoporte> cliente = sService.buscarPorCliente(idCliente);
+        if(cliente.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
         return ResponseEntity.ok(sService.buscarPorCliente(idCliente));
     }
 }
